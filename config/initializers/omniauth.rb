03c92071a18542bef981e417e23268241a67e9a9
fportal-globant <fernando.portal@globant.com>
2014-01-28 20:57:48 -0200

    add omniauth.rb

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET']
+   provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
+end
